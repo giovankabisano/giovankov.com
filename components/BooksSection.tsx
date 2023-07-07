e6a2ae04869bbf88c6141e0234e19f78ad946e8f
giovankabisano <gbisano@gmail.com>
2023-07-07 14:30:19 +0700

    Fix: run time error in books section

--- components/BooksSection.tsx
+++ components/BooksSection.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import Image from "next/image"
 import Link from "next/link"
 import SlideUp from "./SlideUp"
-import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
+import { BsArrowUpRightSquare } from "react-icons/bs"
 
 const projects = [
   {

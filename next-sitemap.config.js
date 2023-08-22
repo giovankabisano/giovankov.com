50cdf848784a68028a3e5e97e6d16641fbb599ab
giovankabisano <gbisano@gmail.com>
2023-08-22 21:29:25 +0700

    Fix: Sitemap siteUrl

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL,
+  siteUrl: 'https://giovanka.vercel.app/',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],

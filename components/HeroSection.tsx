f7e1c57e7144e9effa09f64b7eda996ad5006612
giovankabisano <gbisano@gmail.com>
2023-08-22 17:07:53 +0700

    Add headshot to homepage

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -10,15 +10,15 @@ const HeroSection = () => {
   return (
     <section id="home">
       <div className="flex flex-col animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
-        {/* <div className="md:mt-2 md:w-1/2">
+        <div className="md:mt-2 md:w-1/2 flex flex-row items-center text-center justify-center">
           <Image
-            src="/headshot.png"
+            src="/headshot.jpg"
             alt=""
-            width={325}
-            height={325}
+            width={250}
+            height={250}
             className="rounded-full shadow-2xl"
           />
-        </div> */}
+        </div>
         <div className="md:mt-2 md:w-4/5">
           <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m <span className="font-semibold text-teal-600">Ivan!</span></h1>
           <p className="text-lg mt-4 mb-2 md:text-2xl">
@@ -27,8 +27,11 @@ const HeroSection = () => {
             Software Engineer{" "}
             based in Jakarta, Indonesia.
           </p>
-          <p className="text-lg mt-2 mb-10 md:text-2xl">
+          <p className="text-lg mt-2 mb-5 md:text-2xl">
             I work with <span className="font-semibold text-teal-600">Kotlin (Android)</span> and <span className="font-semibold text-teal-600">React Ecosystem (React JS & React Native)</span>.
+          </p>
+          <p className="text-lg mt-2 mb-5 md:text-2xl">
+            Initiator of <span className="font-semibold text-[#4c99d3]">Peduli Lindungi</span>, recently become <span className="font-semibold text-[#4c99d3]">Satu Sehat</span>.
           </p>
           <p className="text-lg mt-2 mb-10 md:text-2xl">
             Currently working in <b>Kargo Technologies</b> 🚛 🚀 🌕 as <span className="font-semibold text-teal-600">Technical Lead</span>

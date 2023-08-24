1c6f811d101c10f8435a53abedb477d253dfaf49
giovankabisano <gbisano@gmail.com>
2023-08-24 20:23:13 +0700

    Update sitemap to not use indexsitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteUrl: 'https://giovankov.com/',
   generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  sitemapSize: 1000,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
   },

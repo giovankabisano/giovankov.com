e6b8c2838c275f30b247588baac2f562a4b6a78f
giovankabisano <gbisano@gmail.com>
2023-08-26 13:07:24 +0700

    Add base blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image";
+
 export default function Page() {
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-6xl">
-      <p>This is blog page</p>
+      <section id="blog">
+        <div className="flex flex-col animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
+          <p>wefwekjhejkw</p>
+        </div>
+      </section>
     </main>
   )
 }
